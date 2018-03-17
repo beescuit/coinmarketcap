4928318a0858524d7f30bdb4de82c00d6810cd1a
BeeCoding <rafael@beecoding.net>
2018-03-16 23:30:08 -0300

    make color configuration easier

--- prices.js
+++ prices.js
@@ -1,43 +1,12 @@
-var CoinMarketCap = require("node-coinmarketcap");
-var coinmarketcap = new CoinMarketCap();
-var colors = require('colors');
+const CoinMarketCap = require("node-coinmarketcap");
+const coinmarketcap = new CoinMarketCap();
+const colors = require('colors');
+const coincfg = require('./coins.json');
+
 coinmarketcap.multi(coins => {
   coins.getTop(10).forEach(function (coin) {
     var color;
-    switch (coin.symbol) {
-      case "BTC":
-        color = "yellow";
-        break;
-      case "ETH":
-        color = "grey"
-        break;
-      case "BCH":
-        color = "green"
-        break;
-      case "XRP":
-        color = "cyan"
-        break;
-      case "LTC":
-        color = "bold"
-        break;
-      case "ADA":
-        color = "blue"
-        break;
-      case "MIOTA":
-        color = "bold"
-        break;
-      case "DASH":
-        color = "blue"
-        break;
-      case "XEM":
-        color = "cyan"
-        break;
-      case "XMR":
-        color = "red"
-        break;
-      default:
-        color = "yellow";
-    }
+    color = (coincfg[coin.symbol]) ? coincfg[coin.symbol] : coincfg.default
     var sym = coin.symbol;
     if (coin.symbol.length < 5) {
       for (var i = 0; i < 5-coin.symbol.length; i++) {
@@ -47,6 +16,6 @@ coinmarketcap.multi(coins => {
     var h1 = coin.percent_change_1h.includes("-")?`▼ ${coin.percent_change_1h}%`.red : `▲ ${coin.percent_change_1h}%`.green;
     var h24 = coin.percent_change_24h.includes("-")?`▼ ${coin.percent_change_24h}%`.red : `▲ ${coin.percent_change_24h}%`.green;
     var d7 = coin.percent_change_7d.includes("-")?`▼ ${coin.percent_change_7d}%`.red : `▲ ${coin.percent_change_7d}%`.green;
-    console.log(`${sym[color].bold} | ${`$${parseFloat(coin.price_usd).toFixed(2)} USD`.cyan} | 1h: ${h1} | 24h: ${h24} | 7d: ${d7}`);
+    console.log(`${sym[color].bold}\t| ${`$${parseFloat(coin.price_usd).toFixed(2)} USD`.cyan}\t | 1h:${h1}\t| 24h:${h24}\t| 7d:${d7}`);
   })
 });
